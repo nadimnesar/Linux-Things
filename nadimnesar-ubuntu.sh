5b7a29bc80a58b458c9d09344fab0ed6fb3aeb4b
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-05-29 18:24:08 +0600

    Update nadimnesar-ubuntu.sh

--- nadimnesar-ubuntu.sh
+++ nadimnesar-ubuntu.sh
@@ -39,6 +39,7 @@ tput setaf 2;
 echo "Installing fev applications...."
 tput sgr0;
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt update
 sudo snap install telegram-desktop
 sudo apt update
