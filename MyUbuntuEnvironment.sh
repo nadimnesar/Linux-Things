c78b4a03812897f39c262024256da2e301b68ef6
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-05-29 12:49:12 +0600

    Update MyUbuntuEnvironment.sh

--- MyUbuntuEnvironment.sh
+++ MyUbuntuEnvironment.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "hello world"
+echo "This is only for nadimnesar ubuntu 20.4 LTS environment setup."
+echo "If you want you can use, but it's made by author choices."
+echo "Let's start...."
+echo -e "\n"
+
+echo "Uninstalling unnecessary applications...."
+sudo apt-get purge thunderbird
+sudo apt-get purge --auto-remove thunderbird
