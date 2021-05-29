b98e86dcd122836645eccfb78d3843c00d0b5a04
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-05-29 14:07:05 +0600

    Update MyUbuntuEnvironment.sh

--- MyUbuntuEnvironment.sh
+++ MyUbuntuEnvironment.sh
@@ -3,8 +3,6 @@
 echo "This is only for nadimnesar ubuntu 20.4 LTS environment setup."
 echo "If you want you can use, but it's made by author choices."
 echo "Let's start...."
-echo -e "\n"
-
 echo "Updaing...."
 sudo apt update
 sudo apt upgrade
