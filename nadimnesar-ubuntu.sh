dad001e655e5b3340946b673b335fd0752a1b66a
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-05-29 15:08:43 +0600

    Update nadimnesar-ubuntu.sh

--- nadimnesar-ubuntu.sh
+++ nadimnesar-ubuntu.sh
@@ -3,12 +3,19 @@
 
 #!/bin/bash
 
+tput setaf 1;
 echo "This is only for nadimnesar's ubuntu 20.4 LTS environment setup."
 echo "If you want you can use, but it's made by author choices."
 echo "Let's start...."
 echo -e "\n"
 
 echo "Updaing...."
+tput sgr0;
 sudo apt update
 sudo apt upgrade
 sudo apt dist-upgrade
+echo -e "\n"
+
+tput setaf 1;
+echo "Installing restricted extra media codecs...."
+sudo apt install ubuntu-restricted-extras
