f29d59fededd17bac8e540cd09e3ae9d402d6d7d
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-05-29 15:45:59 +0600

    Update nadimnesar-ubuntu.sh

--- nadimnesar-ubuntu.sh
+++ nadimnesar-ubuntu.sh
@@ -31,3 +31,23 @@ echo "Installing gnome tweak...."
 tput sgr0;
 sudo apt install gnome-tweak-tool
 sudo apt install gnome-shell-extensions
+
+tput setaf 2;
+echo "Installing fev applications...."
+tput sgr0;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt update
+sudo snap install telegram-desktop
+sudo apt update
+sudo snap install discord
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update
+sudo apt install sublime-text
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install pycharm-professional --classic
+sudo apt install stacer
+sudo snap install vlc
