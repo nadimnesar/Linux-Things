0aaf007963d48518544d3bbe0a768a2cec6a5c6f
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-08-17 17:03:44 +0600

    Update FORpop21point4.sh

--- FORpop21point4.sh
+++ FORpop21point4.sh
@@ -13,10 +13,30 @@ sudo apt install snapd
 sudo apt install -y gnome-tweak-tool
 sudo apt install -y gnome-shell-extensions
 
+wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
+sudo apt install xz-utils
+tar -xf xdm*
+sudo ./install.sh
+
+sudo apt-get install codeblocks
+
+sudo apt update
+sudo apt install obs-studio
+sudo apt update
+sudo snap install kdenlive
+
+sudo apt install default-jre
+sudo apt install default-jdk
+
+sudo add-apt-repository ppa:maarten-fonville/android-studio
+sudo apt update
+sudo apt install android-studio
+
 sudo apt update
 sudo snap install telegram-desktop
 sudo apt update
 sudo snap install discord
+
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -28,6 +48,9 @@ sudo apt install -y stacer
 sudo apt install -y vlc
 sudo apt update
 sudo apt install qbittorren
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 sudo apt update
 sudo apt install -y build-essential
