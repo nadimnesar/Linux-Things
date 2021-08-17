d989e6b9ccb97fb6f093a90c6f16b119140ae346
Nesar Ahmed <55477231+nadimnesar@users.noreply.github.com>
2021-08-17 17:07:10 +0600

    Update FORpop21point4.sh

--- FORpop21point4.sh
+++ FORpop21point4.sh
@@ -10,8 +10,6 @@ sudo apt dist-upgrade
 sudo apt update
 sudo apt install snapd
 sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 sudo apt update
 sudo apt install -y gnome-tweak-tool
